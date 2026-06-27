3c070170d45bd9a0fbf7102fb5e38dee99427b05
Agrima <agrima0006@gmail.com>
2026-06-27 15:29:17 +0530

    Initial commit

---
+++ src/main/java/com/embarkx/ChallengeApp/ChallengeController.java
@@ -0,0 +1,14 @@
+package embarkx.ChallengeApp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ChallengeController {
+    private List <Challenges> challenges = new ArrayList<>();
+
+
+    public List<Challenges> getAllChallenges() {
+        return challenges;
+    }
+}
---
+++ src/main/java/com/embarkx/ChallengeApp/Challenges.java
@@ -0,0 +1,37 @@
+package embarkx.ChallengeApp;
+
+public class Challenges {
+    private long id;
+    private String month;
+    private String description;
+
+    public Challenges(long id, String month, String description) {
+        this.id = id;
+        this.month = month;
+        this.description = description;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getMonth() {
+        return month;
+    }
+
+    public void setMonth(String month) {
+        this.month = month;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
