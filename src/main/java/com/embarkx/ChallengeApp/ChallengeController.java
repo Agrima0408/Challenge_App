0c992f358e35d99b7c67ad59537b713a30a41fcc
Agrima Agarwal <agrima0006@gmail.com>
2026-08-19 23:24:04 +0530

    Update ChallengeController.java

--- src/main/java/com/embarkx/ChallengeApp/ChallengeController.java
+++ src/main/java/com/embarkx/ChallengeApp/ChallengeController.java
@@ -1,13 +1,21 @@
-package embarkx.ChallengeApp;
+package com.embarkx.ChallengeApp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
 
-
+@RestController
 public class ChallengeController {
     private List <Challenges> challenges = new ArrayList<>();
 
+    public ChallengeController() {
+        Challenges challenge1 = new Challenges(1,"January","build something");
+        challenges.add(challenge1);
+    }
 
+@GetMapping("/challenges")
     public List<Challenges> getAllChallenges() {
         return challenges;
     }
