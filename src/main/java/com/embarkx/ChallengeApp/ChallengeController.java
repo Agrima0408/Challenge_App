9e513eb800efbaa0695b5aa5b32cebb92a3c8c60
Agrima Agarwal <agrima0006@gmail.com>
2026-08-22 23:56:51 +0530

    Update ChallengeController.java
    Added a POST endpoint to accept new challenges.

--- src/main/java/com/embarkx/ChallengeApp/ChallengeController.java
+++ src/main/java/com/embarkx/ChallengeApp/ChallengeController.java
@@ -1,6 +1,8 @@
 package com.embarkx.ChallengeApp;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -8,15 +10,22 @@ import java.util.List;
 
 @RestController
 public class ChallengeController {
-    private List <Challenges> challenges = new ArrayList<>();
+    private List<Challenges> challenges = new ArrayList<>();
 
     public ChallengeController() {
-        Challenges challenge1 = new Challenges(1,"January","build something");
+        Challenges challenge1 = new Challenges(1, "January", "build something");
         challenges.add(challenge1);
     }
 
-@GetMapping("/challenges")
+    @GetMapping("/challenges")
     public List<Challenges> getAllChallenges() {
         return challenges;
     }
+
+    @PostMapping("/challenges")
+    public String addChallenge(@RequestBody Challenges challenge){
+        challenges.add(challenge);
+                return "Challenge accepted successfully";
+    }
+
 }
